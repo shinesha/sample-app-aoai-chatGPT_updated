884358a5549a00a85fe2e2f30409abdf54d0d5cf
Gohil <viran.gohil@fujitsu.com>
2024-01-22 13:37:17 +0000

    interface edit5

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -692,7 +692,7 @@ const Chat = () => {
                                         },
                                         root: {
                                             color: '#FFFFFF',
-                                            background: "radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)",
+                                            background: "#E60012",
                                         },
                                         rootDisabled: {
                                             background: "#F0F0F0"
